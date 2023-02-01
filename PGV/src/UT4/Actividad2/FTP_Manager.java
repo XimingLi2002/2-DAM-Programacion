1feb8560860e0ec2d401aaab5947acf2051dbede
Kiwuh <ximingli0@gmail.com>
2023-02-01 23:00:41 +0000

    PGV updated

--- PGV/src/UT4/Actividad2/FTP_Manager.java
+++ PGV/src/UT4/Actividad2/FTP_Manager.java
@@ -4,32 +4,34 @@ import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class FTP_Manager {
     private FTPClient ftpClient;
-    public FTP_Manager(){
+
+    public FTP_Manager() {
         ftpClient = new FTPClient();
     }
+
     protected void setConnection(String ftpServer) throws IOException {
         ftpClient.connect(ftpServer);
         ftpClient.enterLocalPassiveMode();
         System.out.println(ftpClient.getReplyString());
 
         //Check if the connection has been refused
-        if (!FTPReply.isPositiveCompletion(ftpClient.getReplyCode())){
+        if (!FTPReply.isPositiveCompletion(ftpClient.getReplyCode())) {
             ftpClient.disconnect();
             System.out.println("Connection declined!");
             System.exit(0);
         }
     }
-    protected void setLoginAccount(String username, String password) throws IOException {
-        if (ftpClient.login(username,password)){
-            System.out.println("Login successful!");
-        }else{
-            System.out.printf("Login failed!");
-        }
+
+    protected void setLogin(String username, String password) throws IOException {
+        if (ftpClient.login(username, password)) System.out.println("Login successful!");
+        else System.out.printf("Login failed!");
     }
 
     protected String getWorkingDirectory() throws IOException {
@@ -48,18 +50,20 @@ public class FTP_Manager {
         return ftpClient.listDirectories();
     }
 
-    protected void showCurrentDirectories() throws IOException{
+    protected void showCurrentDirectories() throws IOException {
         FTPFile[] currentDirectories = ftpClient.listDirectories();
-        for(FTPFile directory : currentDirectories){
+        for (FTPFile directory : currentDirectories) {
             System.out.println(directory.getName());
         }
     }
 
     protected void setWorkingDirectory(String path) throws IOException {
-        ftpClient.changeWorkingDirectory("/"+ path);
+        ftpClient.changeWorkingDirectory("/" + path);
     }
+
     private static ArrayList<FTPFile> fileList = new ArrayList<>();
     private static ArrayList<String> directories = new ArrayList<>();
+
     protected ArrayList<FTPFile> getAllFiles(String path) throws IOException {
         do {
             FTPFile[] files = this.getFiles(path);
@@ -79,14 +83,42 @@ public class FTP_Manager {
                 directories.remove(0);
                 getAllFiles(directory);
             }
-        }while(!directories.isEmpty());
+        } while (!directories.isEmpty());
         return fileList;
     }
 
-     protected static void printFormater(int separation, String[] colums) {
+    protected static void printFormater(int separation, String[] colums) {
         for (int i = 0; i < colums.length - 1; i++) {
             System.out.printf("%-" + separation + "s", colums[i]);
         }
         System.out.println(colums[colums.length - 1]);
+    }
+    public boolean makeDir(String pathName) {
+        try {
+            return  ftpClient.makeDirectory(pathName);
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public boolean uploadOneFile(String localPath,String destinationFilePath) {
+        try {
+            BufferedInputStream bi = new BufferedInputStream(new FileInputStream(localPath));
+
+            return ftpClient.storeFile(destinationFilePath, bi);
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    protected boolean isConnected(){
+        return ftpClient.isConnected();
+    }
+    protected void closeConnection(){
+        try {
+            ftpClient.disconnect();
+        } catch (IOException e) {
+            System.err.println("Disconnection failed!");
+        }
     }
 }
--- PGV/src/UT4/Actividad2/Main.java
+++ PGV/src/UT4/Actividad2/Main.java
@@ -1,43 +1,122 @@
 package UT4.Actividad2;
 
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTPReply;
-
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Scanner;
 
 import static UT4.Actividad2.FTP_Manager.printFormater;
 
+//Actividad 2. Subir archivos FTP.
 public class Main implements ConsoleColors {
-    //Actividad 2. Subir archivos FTP.
-    private static final String FTP_SERVER = "192.168.192.190";
-    //private static final String FTP_SERVER = "192.168.10.1";
 
-    //RITA
-    //private static final String FTP_SERVER = "192.168.192.75";
+    //IP de mi PC --> Aula R02 : "192.168.192.190"   Wifi Compartida de Peng: "192.168.10.1"
+    //IP de Rita --> "192.168.192.75"
+    private static final String FTP_SERVER = "192.168.192.190";
     private static final String username = "pgv";
     private static final String password = "pgv";
+        private final static FTP_Manager ftpManager = new FTP_Manager();
+    private final static Scanner scanner = new Scanner(System.in);
+    ;
 
     public static void main(String[] args) throws SocketException, IOException {
-        FTP_Manager ftpManager = new FTP_Manager();
         ftpManager.setConnection(FTP_SERVER);
-        ftpManager.setLoginAccount(username, password);
+        ftpManager.setLogin(username, password);
+        do {
+        System.out.println("Choose an action to execute: (Enter the text between '[' y ']')");
+        System.out.println("[0] Mostrar todos los ficheros de un directorio, incluido los subdirectorios que contenga");
+        System.out.println("[1] Subir un directorio local al servidor");
+        switch (scanner.nextLine()) {
+            case "1":
+                //Clear the arrayList
+                dirFiles = new ArrayList<>();
+                getDirAllFiles(getLocalDir());
+                if (!dirFiles.isEmpty())
+                break;
+            default:
+                System.err.println("No se ha encontrado");
+        }
 
-        //ftpManager.showCurrentDirectories();
-        //System.out.println("Para cambiar de directorio introduzca el nombre del directorio, en caso contrario seguirás en el directorio actual");
-        //ftpManager.setWorkingDirectory(new Scanner(System.in).nextLine());
+        }while (!ftpManager.isConnected());
 
         System.out.print(BOLD);
         printFormater(60, new String[]{"Nombre", "Tipo", "Tama\u00f1o"}); //'\u00f1' = ñ -> Para que se muestra la ñ en consola
         System.out.print(RESET);
-        //ftpManager.getAllFiles("");
+
         ftpManager.getAllFiles("").forEach(it -> printFormater(60, new String[]{it.getName(), "Archivo", String.valueOf(it.getSize())}));
 
 
     }
+
+    private static File getLocalDir() {
+        File dir;
+        boolean checkDirExists;
+        do {
+            System.out.println("Input a local directory absolute path: ");
+            dir = new File(scanner.nextLine());
+            checkDirExists = checkDirExists(dir);
+            if (checkDirExists) {
+                System.out.println("Received!");
+            } else {
+                System.out.println("The directory does not exist! Try Again");
+            }
+        } while (!checkDirExists);
+        return dir;
+    }
+
+    private static boolean checkDirExists(File file) {
+        return (file.isDirectory() && file.isAbsolute() && file.exists());
+    }
+
+    private static ArrayList<File> dirFiles;
+
+    private static void getDirAllFiles(File dir) {
+        for (File file : Objects.requireNonNull(dir.listFiles())) {
+            if (file.isDirectory()) {
+                getDirAllFiles(file);
+            } else {
+                dirFiles.add(file);
+            }
+        }
+    }
+
+    private static void upload() {
+        if (dirFiles.isEmpty()) {
+
+            boolean dirCreated = false;
+
+            do {
+                System.out.println("\nEnter a new remote directory name/path (Ex: dir2 | dir2/subdir2 ): ");
+                String newDir = scanner.nextLine();
+
+                if (!newDir.equals("")) {
+                    dirCreated = ftpManager.makeDir(newDir);
+
+                    if (dirCreated) {
+                        System.out.println("Dir " + newDir + " created!");
+                        System.out.println("\n||==========||Upload status||==========||\n");
+                        int uploadCount = 0;
+
+                        for (File file : dirFiles) {
+                            boolean uploaded = ftpManager.uploadOneFile(file.getAbsolutePath(), "./" + newDir + "/" + file.getName());
+                            System.out.println(file.getName() + " uploaded: " + uploaded);
+
+                            if (uploaded) uploadCount++;
+                        }
+
+                        System.out.println("\nUPLOADED FILES: " + uploadCount);
+                    }
+                }
+            } while (!dirCreated);
+
+        } else {
+            System.out.println("Empty directory, nothing uploaded...");
+        }
+    }
+
 
 }
